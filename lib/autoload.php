6422c4243be230a02de640ec94a1957e2bfa9ebe
Hannelore <56294820+Elyoneondra@users.noreply.github.com>
2020-03-10 11:39:48 +0100

    Add files via upload
    Steek hier je eigen Config.php in!

---
+++ lib/autoload.php
@@ -0,0 +1,39 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . "/groepswerk/02/0/Model/Config.php";
+
+$config = new Config( $path = "/groepswerk/02/0" );
+$_application_folder = $config->getApplicationFolder();
+$_root_folder = $config->getRootFolder();
+
+//load Models
+require_once $_root_folder . "/Model/City.php";
+require_once $_root_folder . "/Model/User.php";
+
+//load Services
+require_once $_root_folder . "/Service/Authentication.php";
+require_once $_root_folder . "/Service/CityLoader.php";
+require_once $_root_folder . "/Service/Container.php";
+require_once $_root_folder . "/Service/DBManager.php";
+require_once $_root_folder . "/Service/MessageService.php";
+require_once $_root_folder . "/Service/UploadService.php";
+require_once $_root_folder . "/Service/DownloadService.php";
+require_once $_root_folder . "/Service/ViewService.php";
+
+session_start();
+
+$Container = new Container( $config );
+
+//rename van veel gebruikte services
+$Auth = $Container->getAuthentication();
+$DBM = $Container->getDBM();
+$MS = $Container->getMessageService();
+$VS = $Container->getViewService();
+
+$_SESSION["head_printed"] = false;
+
+//redirect naar NO ACCESS pagina als de gebruiker niet ingelogd is en niet naar
+//een publiek toegankelijke pagina gaat (login, registratie of no_access)
+if ( ! isset($_SESSION['usr']) AND ! $login_form AND ! $register_form AND ! $no_access )
+{
+    header("Location: " . $config->getApplicationFolder() . "/no_access.php");
+}
